2eecd2ee08b8907f05840beef0f0ada5679c14e5
Shane <shane.duncan56@gmail.com>
2020-01-03 12:58:42 +0100

    added gloss cleaning methods, tests

--- Gloss.py
+++ Gloss.py
@@ -9,6 +9,11 @@ import re
 class Gloss:
     base_url = 'http://nihongo.monash.edu/cgi-bin/wwwjdic?9ZIH%s'
 
+    junk_terms = ['(P);', 'ED;', 'ED', 'KD', 'ES', 'ST', 'SP', 'MA', 'EV', '(n,adj-no)',
+                  '(n)', '(adv-no)', '(adj-no,n)', '(adj-no)',
+                  '(v1)', '(vi)', '(v1,vi)', '( adj-i)', '(adj-na)', '(aux)',
+                  '(aux-v)']
+
     def fetchGlosses(self, term):
         url = self.base_url%parse.quote(term)
         out = request.urlopen(url).read()
@@ -16,7 +21,24 @@ class Gloss:
         ls = re.findall('<li>(.*?)</li>', result)
         return ls
 
-    def clean_gloss_front(self, gloss):
+    def clean_verb_stem(self, gloss):
+        txt = '《verb stem》'
+        verbSectIndex = gloss.find(txt)
+        if verbSectIndex != -1:
+            numSecStart = gloss[verbSectIndex::].find('(1)')
+            if numSecStart != -1:
+                return gloss[0:verbSectIndex] + gloss[verbSectIndex+numSecStart::]
+
+
+            for counter, c in enumerate(gloss[verbSectIndex+len(txt)::]):
+                charVal = ord(c)
+                if (charVal >= 65 and charVal <= 90) or (charVal >= 97 and charVal <= 122):
+                    return gloss[0:verbSectIndex] + gloss[verbSectIndex+len(txt)+counter::]
+
+        return gloss
+
+
+    def clean_front(self, gloss):
         gloss = gloss.strip()
         tabIndex = gloss.find('\t')
         cutIndex = -1
@@ -41,3 +63,29 @@ class Gloss:
 
         cleaned = gloss[0:cutIndex] + ' - ' + gloss[tabIndex + 1::]
         return cleaned
+
+    def remove_dict_annotations(self, gloss):
+        for term in self.junk_terms:
+            index = 0
+
+            while index != -1:
+                index = gloss.find(term)
+                if index != -1:
+                    gloss = gloss[0:index] + gloss[index + len(term)::]
+
+        return gloss
+
+    def clean_back(self, gloss):
+        semicolonIndex = -1
+        for i in range(len(gloss)):
+            curIndex = len(gloss)-i-1
+
+            if gloss[curIndex] == ';':
+                semicolonIndex = curIndex
+                break
+
+        cleaned = gloss[0:semicolonIndex]
+        return cleaned
+
+
+
--- test.py
+++ test.py
@@ -6,12 +6,81 @@ class TestGloss(unittest.TestCase):
         result = gloss.fetchGlosses('よく晴れた夜空')
         self.assertEqual(len(result), 3)
 
-    def test_clean_gloss(self):
+    def test_clean_gloss_front(self):
         gloss = Gloss()
-        result = gloss.fetchGlosses('よく晴れた夜空')
-        clean1 = gloss.clean_gloss_front(result[0])
-        self.assertEqual('よく - nicely; properly; well; skillfully; skilfully; (2) (uk) frequently; often; (3) (uk) I\'m glad that you ...; thank you for ...; KD', clean1)
-        clean2 = gloss.clean_gloss_front(result[1])
-        self.assertEqual('晴れ - clear weather; fine weather; (adj-no,n) (2) (ant: 褻) formal; ceremonial; public; (3) cleared of suspicion; (P);  《verb stem》 晴れる : 晴れる(P); 霽れる 【はれる】 ; (v1,vi) (1) to clear up; to clear away; to be sunny; to stop raining; (2) to refresh (e.g. spirits); (3) (See 疑いが晴れる) to be cleared (e.g. of a suspicion); (4) to be dispelled; to be banished; (P); ED', clean2)
-        clean3 = gloss.clean_gloss_front(result[2])
-        self.assertEqual("夜空 - night sky; (P); ED; Name(s):  【やくう】 (f) Yakuu 【よぞら】 (f) Yozora  SrcHNA", clean3)
+
+        text1 = ' 夜空 【よぞら】 	(n) night sky; (P); ED; Name(s):  【やくう】 (f) Yakuu 【よぞら】 (f) Yozora  SrcHNA '
+        clean1 = gloss.clean_front(text1)
+        self.assertEqual('夜空 - night sky; (P); ED; Name(s):  【やくう】 (f) Yakuu 【よぞら】 (f) Yozora  SrcHNA', clean1)
+
+        text2 = ' 晴れ : 晴れ(P); 晴; 霽れ 【はれ】 	(n,adj-no) (1) (See 快晴・かいせい) clear weather; fine weather;'
+        clean2 = gloss.clean_front(text2)
+        self.assertEqual('晴れ - clear weather; fine weather;', clean2)
+
+        text3 = ' 夜空 【よぞら】 	(n) night sky; (P);'
+        clean3 = gloss.clean_front(text3)
+        self.assertEqual('夜空 - night sky; (P);', clean3)
+
+    def test_clean_verb_stem_withnumbers(self):
+        gloss = Gloss()
+        result = gloss.clean_verb_stem('《verb stem》 晴れる : 晴れる(P); 霽れる 【はれる】 ; (v1,vi) (1) to clear up')
+        self.assertEqual('(1) to clear up', result)
+
+    def test_clean_verb_stem_withoutnumbers(self):
+        gloss = Gloss()
+        inputText = '《verb stem》 晴れる : 晴れる(P); 霽れる 【はれる】 ; (v1,vi) to clear up'
+        #need to clear junk like '(P)' so we find the actual text start
+        inputText = gloss.remove_dict_annotations(inputText)
+        result = gloss.clean_verb_stem(inputText)
+        self.assertEqual('to clear up', result)
+
+
+    def test_clean_gloss_back(self):
+        gloss = Gloss()
+
+        text1 = ' thank you for ...; KD '
+        clean1 = gloss.clean_back(text1)
+        self.assertEqual(' thank you for ...', clean1)
+
+        text2 = '(4) to be dispelled; to be banished; (P); ED '
+        text2 = gloss.remove_dict_annotations(text2)
+        clean2 = gloss.clean_back(text2)
+        self.assertEqual('(4) to be dispelled; to be banished', clean2)
+
+        text3 = ' 夜空 【よぞら】 	(n) night sky; (P); ED; Name(s):  【やくう】 (f) Yakuu 【よぞら】 (f) Yozora  SrcHNA '
+        text3 = gloss.remove_dict_annotations(text3)
+        clean3 = gloss.clean_back(text3)
+        self.assertEqual(' 夜空 【よぞら】 	 night sky', clean3)
+
+    def test_cleanall_1(self):
+        gloss = Gloss()
+
+        text = ' よく  	(adv) (1) nicely; properly; well; skillfully; skilfully; (2) (uk) frequently; often; (3) (uk) I\'m glad that you ...; thank you for ...; KD '
+        clean = gloss.remove_dict_annotations(text)
+        clean = gloss.clean_front(clean)
+        clean = gloss.clean_verb_stem(clean)
+        clean = gloss.clean_back(clean)
+
+        self.assertEqual('よく - nicely; properly; well; skillfully; skilfully; (2) (uk) frequently; often; (3) (uk) I\'m glad that you ...; thank you for ...', clean)
+
+    def test_cleanall_2(self):
+        gloss = Gloss()
+
+        text = ' 晴れ : 晴れ(P); 晴; 霽れ 【はれ】 	(n,adj-no) (1) (See 快晴・かいせい) clear weather; fine weather; (adj-no,n) (2) (ant: 褻) formal; ceremonial; public; (3) cleared of suspicion; (P);  《verb stem》 晴れる : 晴れる(P); 霽れる 【はれる】 ; (v1,vi) (1) to clear up; to clear away; to be sunny; to stop raining; (2) to refresh (e.g. spirits); (3) (See 疑いが晴れる) to be cleared (e.g. of a suspicion); (4) to be dispelled; to be banished; (P); ED '
+        clean = gloss.remove_dict_annotations(text)
+        clean = gloss.clean_front(clean)
+        clean = gloss.clean_verb_stem(clean)
+        clean = gloss.clean_back(clean)
+
+        self.assertEqual('晴れ - clear weather; fine weather;  (2) (ant: 褻) formal; ceremonial; public; (3) cleared of suspicion;   (1) to clear up; to clear away; to be sunny; to stop raining; (2) to refresh (e.g. spirits); (3) (See 疑いが晴れる) to be cleared (e.g. of a suspicion); (4) to be dispelled; to be banished', clean)
+
+    def test_cleanall_3(self):
+        gloss = Gloss()
+
+        text = ' 夜空 【よぞら】 	(n) night sky; (P); ED; Name(s):  【やくう】 (f) Yakuu 【よぞら】 (f) Yozora  SrcHNA '
+        clean = gloss.remove_dict_annotations(text)
+        clean = gloss.clean_front(clean)
+        clean = gloss.clean_verb_stem(clean)
+        clean = gloss.clean_back(clean)
+
+        self.assertEqual('夜空 - night sky', clean)
